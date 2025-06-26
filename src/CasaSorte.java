2b986f869f4c5ecd265fc495d941447eef4d2516
Fabio Azevedo <fabiostahlazevedo@gmail.com>
2025-06-26 01:17:20 -0300

    mudança na lógica das casas

--- src/CasaSorte.java
+++ src/CasaSorte.java
@@ -3,6 +3,9 @@ package src;
 public class CasaSorte extends Casa {
     @Override
     public String aplicarEfeito(Jogador jogador) {
+        if (jogador instanceof Azarado) {
+            return "Jogador azarado não pode tirar cartas de sorte.";
+        }
         Tabuleiro tabuleiro = Tabuleiro.getInstancia();
         tabuleiro.setCasaJogador(jogador.getIndice(), tabuleiro.getCasaJogador(jogador.getIndice()) + 3);
         return "Você tirou uma carta de sorte! Avance 3 casas.";
